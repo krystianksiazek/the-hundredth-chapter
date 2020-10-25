2ecfbc9314a014aa443af7e72fa73f3dcdfc007b
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-10-25 22:54:06 +0100

    Import popperjs/core, bootstrap-vue, jquery and vue-popperjs.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue';
+import $ from 'jquery';
+import BootstrapVue from 'bootstrap-vue'
+import Popper from 'vue-popperjs';
+import 'vue-popperjs/dist/vue-popper.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+Vue.use(Popper)
 
 new Vue({
   router,
   store,
+  $,
   render: (h) => h(App),
 }).$mount('#app');
