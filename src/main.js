0bd9c1a8d07ccef92cef28f3e0d16cb63346ffec
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-06 14:10:47 +0100

    Way of importing libraries has been changed.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import $ from 'jquery';
+import jQuery from 'jquery';
 import BootstrapVue from 'bootstrap-vue'
 import Popper from 'vue-popperjs';
 import 'vue-popperjs/dist/vue-popper.css';
@@ -8,6 +8,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue';
 import router from './router';
 import store from './store';
+global.jQuery = jQuery;
+global.Popper = Popper;
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue)
@@ -16,6 +18,5 @@ Vue.use(Popper)
 new Vue({
   router,
   store,
-  $,
   render: (h) => h(App),
 }).$mount('#app');
