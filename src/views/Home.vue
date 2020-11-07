ae64c3e161cc6359558bd7ac7d6ef343cc84edae
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-07 23:17:01 +0100

    A spinbutton has been added to control the itemsInCart variable.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="homeWrapper">
-    <h1>Home</h1>
+    <h1>Strona główna</h1>
+    <div>
+      <label for="sb-vertical">Dodawanie do koszyka</label><br>
+      <b-form-spinbutton id="sb-vertical" v-model="store.itemsInCart" min="0" max="99" vertical></b-form-spinbutton>
+      <p>Rzeczy w koszyku: {{ store.itemsInCart }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'home',
-  mounted() {
-
+  data () {
+    return {
+    }
+  },
+  methods: {
+    // increase() {
+    //   this.store.itemsInCart += 1;
+    // },
+    // decrease() {
+    //   var $ = require('jquery');
+    //   if(this.store.itemsInCart === 0) {
+    //     $('.decrease').attr('disable');
+    //   } else this.store.itemsInCart -= 1;
+    // }
   }
 }
 </script>
