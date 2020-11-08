078e2af94914c054fc4bd85c57f62b40e1fd1517
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-08 21:59:51 +0100

    Now itemsInCart is updated via the add to cart button.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,9 @@
     <h1>Strona główna</h1>
     <div>
       <label for="sb-vertical">Dodawanie do koszyka</label><br>
-      <b-form-spinbutton id="sb-vertical" v-model="store.itemsInCart" min="0" max="99" vertical></b-form-spinbutton>
-      <p>Rzeczy w koszyku: {{ store.itemsInCart }}</p>
+      <b-form-spinbutton class="quantity" v-model="basketValue" id="sb-vertical" min="0" max="99" inline></b-form-spinbutton><br>
+      <b-button class="addToCart" @click="updateCart()" :disabled="basketValue <= 0">Dodaj do koszyka</b-button>
+      <p>Rzeczy w koszyku: {{ store.itemsInCart }} {{ basketValue }} </p>
     </div>
   </div>
 </template>
@@ -14,22 +15,34 @@ export default {
   name: 'home',
   data () {
     return {
+      basketValue: 0,
     }
   },
   methods: {
-    // increase() {
-    //   this.store.itemsInCart += 1;
-    // },
-    // decrease() {
-    //   var $ = require('jquery');
-    //   if(this.store.itemsInCart === 0) {
-    //     $('.decrease').attr('disable');
-    //   } else this.store.itemsInCart -= 1;
-    // }
-  }
+    updateCart() {
+      this.store.itemsInCart += this.basketValue;
+      this.basketValue = 0;
+    }
+  },
+  // methods: {
+  //   increase() {
+  //     this.store.itemsInCart += 1;
+  //   },
+  //   decrease() {
+  //     var $ = require('jquery');
+  //     if(this.store.itemsInCart === 0) {
+  //       $('.decrease').attr('disable');
+  //     } else this.store.itemsInCart -= 1;
+  //   }
+  // }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.quantity {
+  background-color: #557a95;
+  border: 1px solid #2b3d4b;
+  color: #fff;
+  margin-bottom: 10px;
+}
 </style>
