911ae0ade73751c003c8a175692dce19418f2000
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-15 22:04:04 +0100

    Created modal.vue to adapt it to display book details.

---
+++ src/components/modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="$emit('close-modal')">
+    ID {{ item }}
+    Tytuł {{ this.$store.getters.products[item].title }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    item: {
+      type: Number,
+      required: true,
+    }
+  },
+  data() {
+    return {
+    };
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+
+</style>
