b2f81b3a45b15c0a5b084fd980607ef225e756aa
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-15 22:08:57 +0100

    Books description field added and unnecessary functions removed.

--- src/store/products.js
+++ src/store/products.js
@@ -7,99 +7,91 @@ export default new Vuex.Store({
   state: {
     products: [
     {
-      id: 1,
+      id: 0,
       title: "Harry Potter i Komnata Tajemnic ",
       cover: "https://cdn-lubimyczytac.pl/upload/books/308000/308636/532903-352x500.jpg",
       price: 22.13,
       flag: "new",
       genere: "fantasy",
       rate: "8,5",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
     {
-      id: 2,
+      id: 1,
       title: "Harry Potter i więzień Azkabanu",
       cover: "https://cdn-lubimyczytac.pl/upload/books/308000/308637/532904-352x500.jpg",
       price: 22.30,
       flag: "new",
       genere: "fantasy",
       rate: "8,3",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
     {
-      id: 3,
+      id: 2,
       title: "Harry Potter i Czara Ognia ",
       cover: "https://cdn-lubimyczytac.pl/upload/books/308000/308633/494236-352x500.jpg",
       price: 20.43,
       flag: "new",
       genere: "fantasy",
       rate: "9,3",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
     {
-      id: 4,
+      id: 3,
       title: "Harry Potter i Zakon Feniksa",
       cover: "https://cdn-lubimyczytac.pl/upload/books/308000/308635/532902-352x500.jpg",
       price: 25.11,
       flag: "new",
       genere: "fantasy",
       rate: "7,3",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
     {
-      id: 5,
+      id: 4,
       title: "Harry Potter i Książę Półkrwi",
       cover: "https://cdn-lubimyczytac.pl/upload/books/308000/308638/494222-352x500.jpg",
       price: 19.79,
       flag: "new",
       genere: "fantasy",
       rate: "8,9",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
     {
-      id: 6,
+      id: 5,
       title: "Harry Potter i Insygnia Śmierci ",
       cover: "https://cdn-lubimyczytac.pl/upload/books/310000/310990/494239-352x500.jpg",
       price: 25.11,
       flag: "new",
       genere: "fantasy",
       rate: "8,1",
-      quantityInCart: 0
+      quantityInCart: 0,
+      description: "Harry po pełnym przygód roku w Hogwarcie spędza nudne wakacje u Dursleyów i z utęsknieniem wyczekuje powrotu do szkoły. Sprawy jednak znacznie się komplikują, gdy pewnego dnia odwiedza go tajemniczy przybysz i ostrzega przed… powrotem do Szkoły Magii i Czarodziejstwa, gdzie ma dojść do strasznych wydarzeń. Czy Harry posłucha ostrzeżenia? Co złego ma się wydarzyć w Hogwarcie? Jakie tajemnice skrywa rodzina Malforya? I najważniejsze – czym jest i gdzie znajduje się tytułowa Komnata Tajemnic?"
     },
   ],
-  StoreCart: [],
-  duplicatesInCart: 0
 },
   mutations: {
     ADD_TO_CART(state, { id, amount }) {
-      // for(let i = 1; i<=amount; i++) {
-        state.products[id-1].quantityInCart += amount;
-        // state.StoreCart.push(id);
-      // }
+      state.products[id].quantityInCart += amount;
     },
-    REMOVE_FROM_CART(state, { index, id }) {
-      state.StoreCart.splice(index, 1);
-      state.products[id-1].quantityInCart -= 1;
-    },
-    REMOVE_ALL_FROM_CART(state) {
-      state.StoreCart.splice(0, state.StoreCart.length);
+    REMOVE_FROM_CART(state, id) {
+      state.products[id].quantityInCart -= 1;
     },
   },
   actions: {
     addToCart(context, { id, amount }) {
       context.commit("ADD_TO_CART", { id, amount });
     },
-    removeFromCart(context, { index, id }) {
-      context.commit("REMOVE_FROM_CART", { index, id });
-    },
-    removeAllFromCart(context) {
-      context.commit("REMOVE_ALL_FROM_CART");
+    removeFromCart(context, id) {
+      context.commit("REMOVE_FROM_CART", id);
     },
   },
   getters: {
     products: (state) => state.products,
-    StoreCart: (state) => state.StoreCart,
-    duplicatesInCart: (state) => state.duplicatesInCart,
   },
   modules: {
   },
