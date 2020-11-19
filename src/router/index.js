3baba224ad1a5db48bf60f6705c4054d1830a5d5
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-19 23:32:09 +0100

    The categories view is now active.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import home from '../views/home.vue';
 import login from '../views/login.vue';
 import register from '../views/register.vue';
+import categories from '../views/categories.vue';
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/register',
     name: 'register',
     component: register,
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: categories,
   },
 ];
 
