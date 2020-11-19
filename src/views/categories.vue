9ced4690db1b3fa33f841018e54b9127b7f3fe5e
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-19 23:33:32 +0100

    The categories view is created.

---
+++ src/views/categories.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="categoriesWrapper">
+    <h1>Categories</h1>
+    
+    <b-button @click="getProducts('fantasy')">Beletrystyka</b-button>
+    <span v-if="products.length != 0">{{ products }}</span>
+    fantastyka, fantasy, science fiction
+    horror
+    klasyka
+    kryminał, sensacja, thriller
+    literatura młodzieżowa
+    literatura obyczajowa, romans
+    literatura piękna
+    powieść historyczna
+    powieść przygodowa
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'categories',
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProducts(genere) {
+      for(let i = 0; i < this.$store.getters.products.length; i++) {
+        if(this.$store.getters.products[i].genere == genere) {
+          this.products += this.$store.getters.products[i].title;
+        }
+      }
+    }
+  }
+};
+</script>
