83dc8d7b135fd31df411ba6549a320d1c99caf33
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-11-27 14:17:35 +0100

    A lot of categories have been added (not all will be) and an initial display has been established.

--- src/views/categories.vue
+++ src/views/categories.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="categoriesWrapper">
     <h1>Categories</h1>
-    
-    <b-button @click="getProducts('fantasy')">Beletrystyka</b-button>
+    <div v-for="category in categories" :key="category.cat">
+      <ul>
+        <li @click="getProducts(category.cat)"><strong>{{category.cat}}</strong></li>
+          <ul>
+            <li v-for="subCat in category.subCat" :key="subCat">
+              <router-link :to="{ name: 'category', params: { id: subCat}}">
+                {{subCat}}
+              </router-link>
+            </li>
+          </ul>
+      </ul>
+    </div>
+    <b-button @click="getProducts('Fantastyka')">Beletrystyka</b-button>
     <span v-if="products.length != 0">{{ products }}</span>
-    fantastyka, fantasy, science fiction
-    horror
-    klasyka
-    kryminał, sensacja, thriller
-    literatura młodzieżowa
-    literatura obyczajowa, romans
-    literatura piękna
-    powieść historyczna
-    powieść przygodowa
-
   </div>
 </template>
 
@@ -23,16 +24,113 @@ export default {
   name: 'categories',
   data () {
     return {
+      categories: [
+        {
+          cat: "Beletrystyka",
+          subCat: [
+            "fantastyka, fantasy, science fiction", 
+            "horror", 
+            "klasyka", 
+            "kryminał, sensacja, thriller",
+            "literatura młodzieżowa",
+            "literatura obyczajowa, romans",
+            "literatura piękna",
+            "powieść historyczna",
+            "powieść przygodowa",
+          ]
+        },
+        {
+          cat: "Literatura faktu, publicystyka",
+            subCat: [
+              "biografia, autobiografia, pamiętnik", 
+              "literatura faktu", 
+              "literatura podróżnicza", 
+              "publicystyka literacka, eseje",
+            ]
+        },
+        {
+          cat: "Literatura popularnonaukowa",
+            subCat: [
+              "astronomia, astrofizyka", 
+              "biznes, finanse", 
+              "encyklopedie, słowniki", 
+              "filozofia, etyka",
+              "flora i fauna",
+              "historia",
+              "informatyka, matematyka",
+              "językoznawstwo, nauka o literaturze",
+              "nauki przyrodnicze (fizyka, chemia, biologia, itd.)",
+              "nauki społeczne (psychologia, socjologia, itd.)",
+              "popularnonaukowa",
+              "poradniki dla rodziców",
+              "technika",
+              "zdrowie, medycyna",
+            ]
+        },
+        {
+          cat: "Literatura dziecięca",
+            subCat: [
+              "bajki", 
+              "baśnie, legendy, podania", 
+              "historie biblijne", 
+              "interaktywne, obrazkowe, edukacyjne",
+              "opowiadania, powieści dla dzieci",
+              "opowieści dla młodszych dzieci",
+              "popularnonaukowa dziecięca",
+              "wierszyki, piosenki",
+              "pozostałe",
+            ]
+        },
+        {
+          cat: "Komiksy",
+            subCat: [
+              "komiksy",
+            ]
+        },
+        {
+          cat: "Poezja, dramat, satyra",
+            subCat: [
+              "poezja",
+              "satyra",
+              "utwór dramatyczny (dramat, komedia, tragedia)",
+            ]
+        },
+        {
+          cat: "Pozostałe",
+            subCat: [
+              "albumy",
+              "czasopisma",
+              "ezoteryka, senniki, horoskopy",
+              "film, kino, telewizja",
+              "hobby",
+              "inne",
+              "kulinaria, przepisy kulinarne",
+              "militaria, wojskowość ",
+              "motoryzacja",
+              "muzyka",
+              "poradniki",
+              "religia",
+              "rozrywka",
+              "rękodzieło",
+              "sport",
+              "sztuka",
+              "teatr",
+              "turystyka, mapy, atlasy",
+            ]
+        },
+      ],
       products: []
     }
   },
   methods: {
     getProducts(genere) {
+      let insideProducts = [];
       for(let i = 0; i < this.$store.getters.products.length; i++) {
         if(this.$store.getters.products[i].genere == genere) {
-          this.products += this.$store.getters.products[i].title;
+          insideProducts += this.$store.getters.products[i].title;
         }
       }
+      this.products = insideProducts;
     }
   }
 };
