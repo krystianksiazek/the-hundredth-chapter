4e2e7066d64fb904a55f1683b86f9c003d4232ba
Krystian Książek <krystianksiazek@users.noreply.github.com>
2020-12-18 11:17:38 +0100

    Remove props that passing to product component, now is get by getters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,17 @@
 <div>
   <Modal v-if="modalOpen" :id = "modalData" @close-modal="modalOpen = false;" />
   <h1>Produkty</h1>
-  <b-container>
-    <b-row class="justify-content-center">
-      <Product
-        v-for="product in products"
-        :key="product.id"
-        :id="product.id"
-        :title="product.title"
-        :author="product.author"
-        :cover="product.cover"
-        :price="product.price"
-        :flag="product.flag"
-        :genere="product.genere"
-        :rate="product.rate"
-        :quantityInCart="product.quantityInCart"
-        :favorite="product.favorite"
-        :description="product.description"
-        :sendModalOpen="modalOpen"
-        @run-modal="runModal"
-      />
-    </b-row>
-  </b-container>
-  
+  <Product @run-modal="runModal" />
 </div>
 </template>
 
 <script>
 import Product from '@/components/product.vue';
-import Modal from '@/components/modal.vue';
 
 export default {
   name: 'home',
   components: {
     Product,
-    Modal
   },
   data() {
     return {
@@ -49,28 +26,14 @@ export default {
     },
   },
   methods: {
-    runModal: function(id) {
+    runModal(id) {
       this.modalOpen = true;
       this.modalData = id;
-    }
+    },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.quantity {
-  background-color: #557a95;
-  border: 1px solid #2b3d4b;
-  color: #fff;
-  margin-bottom: 10px;
-}
-.homeWrapper {
-  display: grid;
-  grid-template-columns: auto auto auto auto auto auto;
-  grid-template-rows: auto auto; 
-  column-gap: 10px;
-  row-gap: 15px;
-  align-items: stretch;
-  justify-items: stretch;
-}
+
 </style>
