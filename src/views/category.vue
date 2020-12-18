8bebdec406d8fa45de22724ea2db0f50c99c518b
Krystian Książek <krystianksiazek@users.noreply.github.com>
2020-12-18 11:16:38 +0100

    Refactor code

--- src/views/category.vue
+++ src/views/category.vue
@@ -1,104 +1,158 @@
 <template>
   <div>
-    <h3>{{ category }}</h3>
-    <div v-for="(product, index) in products" :key="product.id">
-    <div class="productWrapper">
-    <div @click="$emit('run-modal', product.id)">
-      <div class="bookTitle">{{ product.title }}</div>
-      <div class="hoverImage">
-        <div v-if="hover === product.id" class="hoverGenere">{{ product.genere }}</div>
-        <img @mouseover="hover = product.id" @mouseleave="hover = null" :src="product.cover" height="250" />
-        <div v-if="hover === product.id" class="hoverRate"><img src="../assets/Icons/star-fill.png" height="20" alt=""> {{ product.rate }}/5</div>
+    <Modal v-if="modalOpen" :id = "modalData" @close-modal="modalOpen = false;" />
+    <h3 v-if="!componentLoading">{{ prefix + ' => ' + suffix }} ({{products.length}})</h3>
+    <h4 v-if="componentLoading">Ładowanie...</h4>
+    <h4 v-if="!componentLoading && products.length === 0">Brak książek tej katergorii</h4>
+    <b-container>
+    <b-row class="justify-content-center">
+     <div v-for="(product, index) in products" :key="product.id">
+      <div class="productWrapper">
+        <div @click="runModal(product.id)">
+          <div class="bookTitle">{{ product.title }}</div>
+          <div class="hoverImage">
+            <div v-if="hover === product.id" class="hoverGenere">{{ product.genere }}</div>
+            <img @mouseover="hover = product.id"
+              @mouseleave="hover = null"
+              :src="product.cover"
+              height="250" />
+            <div v-if="hover === product.id" class="hoverRate">
+              <img src="../assets/Icons/star-fill.png" height="20" alt="">
+                {{ product.rate }}/5
+            </div>
+          </div>
+        </div>
+        {{ product.author }}
+        <div class="addToCartSection">
+          <span class="bookPrice">{{ product.price.toFixed(2) + " zł" }}</span>
+          <b-form-spinbutton
+            class="quantity"
+            v-model="basketValue[index]"
+            id="sb-small" min="1" max="99">
+          </b-form-spinbutton>
+          <b-button
+            :id="'addToCartBtn' + product.id"
+            class="addToCart"
+            @click="addToCart(product.id, basketValue[index], index)"
+            :disabled="basketValue[index] <= 0">
+            <img class="addToCartIco" src="../assets/Icons/basket-green.png" height="30" alt="">
+          </b-button>
+          <b-tooltip
+            :target="'addToCartBtn' + product.id"
+            placement="bottomleft"
+            variant="success"
+            triggers="hover"
+            :delay="{show: 800, hide: 50}"
+            noninteractive>
+            <strong>Dodaj do koszyka</strong>
+          </b-tooltip>
+          <b-button
+            :id="'addToFavoritesBtn' + product.id"
+            v-bind:style = "[product.favorite ? {'title':'Dodaj do ulubionych'} : {'title':'Usuń z ulubionych'}]"
+            @click="favoriteToggle(product.id)"
+            class="addToFavorites">
+            <img v-if="!product.favorite" class="addToFavoritesIco" src="../assets/Icons/heart-red.png" height="30" alt="">
+            <img v-if="product.favorite" class="addToFavoritesIco" src="../assets/Icons/heart-red-fill.png" height="30" alt="">
+          </b-button>
+          <b-tooltip
+            :target="'addToFavoritesBtn' + product.id"
+            placement="bottomright"
+            variant="danger"
+            triggers="hover"
+            :delay="{show: 800, hide: 50}"
+            noninteractive>
+            <span v-if="!product.favorite"><strong>Dodaj do ulubionych</strong></span>
+            <span v-if="product.favorite"><strong>Usuń z ulubionych</strong></span>
+          </b-tooltip>
+        </div>
       </div>
     </div>
-    {{ product.author }}
-    <div class="addToCartSection">
-      <span class="bookPrice">{{ product.price.toFixed(2) + " zł" }}</span>
-      <b-form-spinbutton class="quantity" v-model="basketValue[index]" id="sb-small" min="1" max="99"></b-form-spinbutton>
-      <b-button
-        :id="'addToCartBtn' + product.id"
-        class="addToCart"
-        @click="addToCart(product.id, basketValue[index], index)" 
-        :disabled="basketValue[index] <= 0">
-        <img class="addToCartIco" src="../assets/Icons/basket-green.png" height="30" alt="">
-      </b-button>
-      <b-tooltip :target="'addToCartBtn' + product.id" placement="bottomleft" variant="success" triggers="hover" :delay="{show: 800, hide: 50}" noninteractive>
-        <strong>Dodaj do koszyka</strong>
-      </b-tooltip>
-      <b-button 
-        :id="'addToFavoritesBtn' + product.id"
-        v-bind:style= "[product.favorite ? {'title':'Dodaj do ulubionych'} : {'title':'Usuń z ulubionych'}]" 
-        @click="favoriteToggle(product.id)" 
-        class="addToFavorites">
-        <img v-if="!product.favorite" class="addToFavoritesIco" src="../assets/Icons/heart-red.png" height="30" alt="">
-        <img v-if="product.favorite" class="addToFavoritesIco" src="../assets/Icons/heart-red-fill.png" height="30" alt="">
-      </b-button>
-      <!-- <b-tooltip :target="'addToFavoritesBtn' + id" placement="bottomright" variant="danger" triggers="hover" :delay="{show: 800, hide: 50}" noninteractive>
-        <span v-if="!favorite"><strong>Dodaj do ulubionych</strong></span>
-        <span v-if="favorite"><strong>Usuń z ulubionych</strong></span>
-      </b-tooltip> -->
-    </div>
-  </div>
-  </div>
+    </b-row>
+  </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "category",
-  data () {
+  name: 'category',
+  data() {
     return {
       basketValue: [],
-      hover: null
-    }
+      hover: null,
+      componentLoading: true,
+      prefix: '',
+      suffix: '',
+      modalOpen: false,
+      modalData: null,
+    };
   },
   props: {
-    myprop: String
+    myprop: String,
   },
   mounted() {
-    this.setBasketValue();
     window.scrollTo(0, 0);
+    this.category();
   },
   computed: {
-    category() {
-      var split = this.$route.params.id.split('.').join('');
-      var category = split[0]-1;
-      if(split[2] === undefined) {
-        var subCategory = split[1]-1;
-      } else {
-        var subCategory = (split[1] + split[2]) - 1;
-      }
-      var prefix = this.$store.getters.getCategories[category].cat;
-      var suffix = this.$store.getters.getCategories[category].subCat[subCategory];
-      return prefix + " => " + suffix;
+    categories() {
+      return this.$store.getters.categories || null;
     },
     book() {
       return this.$store.getters.products;
     },
     products() {
-      let productReturn = [];
-      for(let i = 0; i < this.book.length; i++) {
-        if(((this.category).toLowerCase()).includes((this.book[i].genere).toLowerCase())) {
+      const productReturn = [];
+      for (let i = 0; i < this.book.length; i++) {
+        if (((this.suffix).toLowerCase()).includes((this.book[i].genere).toLowerCase())) {
           productReturn[i] = this.book[i];
         }
       }
       return productReturn.flat();
-    }
+    },
   },
   methods: {
+    runModal(id) {
+      this.modalOpen = true;
+      this.modalData = id;
+    },
+    category() {
+      let subCategory = null;
+      const split = this.$route.params.id.split('.').join('');
+      const category = split[0] - 1;
+      if (split[2] === undefined) {
+        subCategory = split[1] - 1;
+      } else {
+        subCategory = (split[1] + split[2]) - 1;
+      }
+      setTimeout(() => {
+        this.prefix = this.categories[category].cat;
+        this.suffix = this.categories[category].subCat[subCategory];
+        this.componentLoading = false;
+        this.setBasketValue();
+      }, 1500);
+    },
     setBasketValue() {
-      this.basketValue = [];
-      for(let i = 0; i < this.products.length; i++) {
+      for (let i = 0; i < this.products.length; i++) {
         this.basketValue.push(1);
       }
     },
     addToCart(id, amount, index) {
-      this.$store.dispatch("addToCart", { id, amount });
+      console.log(amount);
+      this.$store.dispatch('addToCart', { id, amount });
       this.basketValue[index] = 1;
       this.$forceUpdate();
     },
     favoriteToggle(id) {
-      this.$store.dispatch("addToFavorite", id);
+      this.$store.dispatch('addToFavorite', id);
+    },
+    getIndex(id) {
+      for (let i = 0; i < this.categories.length; i++) {
+        for (let j = 0; j < this.categories[i].subCat.length; j++) {
+          if (this.categories[i].subCat[j] === id) {
+            return `${i + 1}.${j + 1}`;
+          }
+        }
+      }
     },
   },
 };
