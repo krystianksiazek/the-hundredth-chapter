957e8e72dfdd2ab0d63f8868dd3a9edf27e3840d
Krystian Książek <krystianksiazek@users.noreply.github.com>
2020-12-26 23:57:48 +0100

    Add view products using the product component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,25 @@
 <div>
   <Modal v-if="modalOpen" :id = "modalData" @close-modal="modalOpen = false;" />
   <h1>Produkty</h1>
-  <Product @run-modal="runModal" />
+  <div v-for="(book, index) in books" :key="book.id">
+    <Product :book='book' :index='index'/>
+  </div>
 </div>
 </template>
 
 <script>
-import Product from '@/components/product.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'home',
-  components: {
-    Product,
-  },
   data() {
     return {
       modalOpen: false,
       modalData: null,
     };
+  },
+  computed: {
+    ...mapGetters(['books', 'categories', 'favorites']),
   },
   methods: {
     runModal(id) {
