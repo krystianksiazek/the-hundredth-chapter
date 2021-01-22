c8d03ba0d6648e6b8ecfcd215aed0078a092af27
Krystian Książek <krystianksiazek@users.noreply.github.com>
2021-01-22 20:43:24 +0100

    Add show filters button and add pagination

--- src/views/rate.vue
+++ src/views/rate.vue
@@ -1,48 +1,65 @@
 <template>
-<div>
-  <div class="bestBooksWrapper">
-    <h1>Najlepiej oceniane książki</h1>
-    <div class="filters">
-      <label for="sb-step">Ocena powyżej: </label>
-      <b-form-spinbutton
-        class="rateSpinbutton"
-        id="sb-small"
-        v-model="rateFilterSelect"
-        min="0"
-        max="4.9"
-        step="0.10">
-      </b-form-spinbutton>
-      <!-- <b-dropdown>
-        <template #button-content>
-          Ocena powyżej: <strong>{{ rateFilterSelect }}</strong>
-        </template>
-        <b-dropdown-item v-for="option in rateFilter" 
-                        :key="option" 
-                        :value="option"
-                        @click="rateFilterSelect = option">
-          {{ option }}
-        </b-dropdown-item>
-      </b-dropdown> -->
-      <b-dropdown class="ml-2">
-        <template #button-content>
-          Sortowanie wg: <strong>{{ sortingSelect }}</strong>
-        </template>
-        <b-dropdown-item v-for="option in sorting" 
-                        :key="option" 
-                        :value="option"
-                        @click="sortingSelect = option">
-          {{ option }}
-        </b-dropdown-item>
-      </b-dropdown>
-    </div>
-    <!-- <b-form-select v-model="rateFilterSelect" :options="rateFilter"></b-form-select> -->
-    <div class="bestBooks">
-      <div v-for="(book, index) in getBestBooksFilter" :key="book.id">
-        <Product :book='book' :index='index'/>
+  <div>
+    <div class="bestBooksWrapper">
+      <h1>Najlepiej oceniane książki</h1>
+      <div v-b-toggle.filters>
+        <span class="when-open">Ukryj</span><span class="when-closed">Pokaż</span> filtry
       </div>
+      <div class="filtersWrapper">
+        <b-collapse id="filters">
+          <div class="filters">
+            <label for="sb-step">Ocena powyżej: </label>
+            <b-form-spinbutton
+              class="rateSpinbutton"
+              id="sb-small"
+              v-model="rateFilterSelect"
+              min="0"
+              max="4.9"
+              step="0.10">
+            </b-form-spinbutton>
+            <!-- <b-dropdown>
+              <template #button-content>
+                Ocena powyżej: <strong>{{ rateFilterSelect }}</strong>
+              </template>
+              <b-dropdown-item v-for="option in rateFilter" 
+                              :key="option" 
+                              :value="option"
+                              @click="rateFilterSelect = option">
+                {{ option }}
+              </b-dropdown-item>
+            </b-dropdown> -->
+            <b-dropdown class="ml-2">
+              <template #button-content>
+                Sortowanie wg: <strong>{{ sortingSelect }}</strong>
+              </template>
+              <b-dropdown-item v-for="option in sorting" 
+                              :key="option" 
+                              :value="option"
+                              @click="sortingSelect = option">
+                {{ option }}
+              </b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </b-collapse>
+      </div>
+      </div>
+      <!-- <b-form-select v-model="rateFilterSelect" :options="rateFilter"></b-form-select> -->
+      <div class="overflow-auto">
+        <div id="bestBooks" class="bestBooks">
+          <div v-for="(book, index) in itemsForList" :key="book.id">
+            <Product :book='book' :index='index'/>
+          </div>
+        </div>
+      </div>
+      <b-pagination
+          v-model="currentPage"
+          :total-rows="getBestBooksFilter.length"
+          :per-page="perPage"
+          aria-controls="bestBooks"
+          first-number
+          last-number>
+      </b-pagination>
     </div>
-  </div>
-</div>
 </template>
 
 <script>
@@ -52,8 +69,10 @@ export default {
   name: 'home',
   data() {
     return {
+      perPage: 6,
+      currentPage: 1,
       rateFilterSelect: 4,
-      sortingSelect: 'domyślnie',
+      sortingSelect: 'oceny malejąco',
       sorting: [
         'oceny rosnąco',
         'oceny malejąco',
@@ -93,15 +112,29 @@ export default {
           break;
       }
     },
+    itemsForList() {
+      return this.getBestBooksFilter.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage);
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
+  .collapsed > .when-open,
+  .not-collapsed > .when-closed {
+    display: none;
+  }
   h1 {
     text-align: center;
   }
   .bestBooks {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .filtersWrapper {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
