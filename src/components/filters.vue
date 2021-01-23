305d2221f9667cca0849514d0e8880797daf4b96
Krystian Książek <krystianksiazek@users.noreply.github.com>
2021-01-24 00:08:17 +0100

    Create filters.vue

---
+++ src/components/filters.vue
@@ -0,0 +1,135 @@
+<template>
+  <b-container>
+    <div v-b-toggle.filters>
+      <span class="when-open">Ukryj</span><span class="when-closed">Pokaż</span> filtry
+    </div>
+    <div class="filtersWrapper">
+      <b-collapse id="filters">
+        <div class="filters">
+          <label for="sb-step">Ocena powyżej: </label>
+          <b-form-spinbutton
+            class="rateSpinbutton"
+            id="sb-small"
+            v-model="rateFilterSelect"
+            min="0"
+            max="4.9"
+            step="0.10">
+          </b-form-spinbutton>
+          <!-- <b-dropdown>
+            <template #button-content>
+              Ocena powyżej: <strong>{{ rateFilterSelect }}</strong>
+            </template>
+            <b-dropdown-item v-for="option in rateFilter" 
+                            :key="option" 
+                            :value="option"
+                            @click="rateFilterSelect = option">
+              {{ option }}
+            </b-dropdown-item>
+          </b-dropdown> -->
+          <b-dropdown class="ml-2">
+            <template #button-content>
+              Sortowanie wg: <strong>{{ sortingSelectSend }}</strong>
+            </template>
+            <b-dropdown-item v-for="option in sorting" 
+                            :key="option" 
+                            :value="option"
+                            @click="sendBackSorting(option)">
+              {{ option }}
+            </b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </b-collapse>
+    </div>
+    <!-- <b-form-select v-model="rateFilterSelect" :options="rateFilter"></b-form-select> -->
+  </b-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      sorting: [
+        'oceny rosnąco',
+        'oceny malejąco',
+        'tytyłu rosnąco',
+        'tytyłu malejąco',
+        'ceny rosnąco',
+        'ceny malejąco',
+        'domyślnie',
+      ],
+    };
+  },
+  props: {
+    sortingSelectSend: {
+      type: String,
+      required: true,
+    },
+    rateFilterSelectSend: {
+      type: Number,
+      required: true,
+    }
+  },
+  methods: {
+    sendBackSorting(sortingSelectSend) {
+      this.$emit("update-sorting", sortingSelectSend);
+    },
+  },
+  computed: {
+    rateFilterSelect: {
+      get(){
+        return this.rateFilterSelectSend;
+      },
+      set(rateFilterSelectSend){
+        this.$emit("update-rate", rateFilterSelectSend);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .collapsed > .when-open,
+  .not-collapsed > .when-closed {
+    display: none;
+  }
+  h1 {
+    text-align: center;
+  }
+  .bestBooks {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .filtersWrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .allBooks {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .pagination {
+    display: flex;
+    justify-content: center;
+  }
+</style>
+<style lang="scss">
+//UNSCOPED CSS!
+  .dropdown-menu {
+    text-align: center;
+    width: 100%;
+  }
+  .rateSpinbutton {
+    width: 150px;
+    position: relative;
+  }
+  .filters {
+    display: flex;
+    align-items: center;
+  }
+</style>
