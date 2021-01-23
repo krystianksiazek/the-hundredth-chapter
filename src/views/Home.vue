207aea130836111f06f6f9396a5d0ee53cdfd33a
Krystian Książek <krystianksiazek@users.noreply.github.com>
2021-01-24 00:11:13 +0100

    Add b-container to wrap all elements in template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,24 @@
 <template>
-<div>
-  <div class="bestBooksWrapper">
-    <h1>Niektóre najlepiej oceniane</h1>
-    <h6>Książki z oceną co najmniej <strong>{{ rateFilterSelect }}</strong></h6>
-    <div class="bestBooks">
-      <div v-for="(book, index) in getBestBooks" :key="book.id">
+  <b-container>
+    <div class="bestBooksWrapper">
+      <h1>Niektóre najlepiej oceniane</h1>
+      <h6>Książki z oceną co najmniej <strong>{{ rateFilterSelect }}</strong></h6>
+      <div class="bestBooks">
+        <div v-for="(book, index) in getBestBooks" :key="book.id">
+          <Product :book='book' :index='index'/>
+        </div>
+      </div>
+      <router-link :to="'/najlepiej-oceniane'">
+        Wszystkie najlepiej oceniane
+      </router-link>
+    </div>
+    <h1>Wszystkie książki</h1>
+    <div class="allBooks">
+      <div v-for="(book, index) in books" :key="book.id">
         <Product :book='book' :index='index'/>
       </div>
     </div>
-    <router-link :to="'/najlepiej-oceniane'">
-      Wszystkie najlepiej oceniane
-    </router-link>
-  </div>
-  <h1>Wszystkie książki</h1>
-  <div class="allBooks">
-    <div v-for="(book, index) in books" :key="book.id">
-      <Product :book='book' :index='index'/>
-    </div>
-  </div>
-</div>
+  </b-container>
 </template>
 
 <script>
@@ -28,7 +28,7 @@ export default {
   name: 'home',
   data() {
     return {
-      rateFilterSelect: (Math.random() * (5 - 4) + 4).toFixed(2),
+      rateFilterSelect: (Math.random() * (4.90 - 4) + 4).toFixed(2),
     };
   },
   computed: {
